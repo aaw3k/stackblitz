df5bf4c07f7f30944bf9581624e787d80a9205a9
Sławek Jaskulski <Blejder4k@gmail.com>
2023-03-27 10:59:19 +0200

    Fix for loop condition in filterButtons

--- web-platform/gradient-border/script.js
+++ web-platform/gradient-border/script.js
@@ -2,14 +2,14 @@ document.querySelectorAll('.item').forEach((color) => {
   color.addEventListener('click', (e) => {
     const filterButtons = document.querySelectorAll('.active')
 
-    for (let i = 0; i < !color.length; i++) {
+    for (let i = 0; i < filterButtons.length; i++) {
       const colorSwatch = e.target
       const colorPreview = colorSwatch.dataset.gradient
 
       colorSwatch.parentNode.previousElementSibling.setAttribute(
         'data-gradient',
         colorPreview
-      )
+      );
     }
 
     filterButtons.forEach((item) => {
